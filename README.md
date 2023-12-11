6ba234515aeeb5179b50d98202be70d270c88c0d
Henrique Cominato <henriquecominato@gmail.com>
2023-12-10 22:26:43 -0300

    server test

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 "# alfazelo" 
-"# alfazelo" 
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Em Construção')
-})
+  return res.json({message:'Server is up'});
+});
+
+app.post('/test',(req,res) => {
+  const {name, date} = req.body;
+  return res.json({name, date});
+});
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
